288565a1e7b17b197e8d1f02c5923fa38c5ba439
Startliu <2821958956@qq.com>
2018-04-18 16:37:41 +0800

    first commit

---
+++ app/src/main/java/lxd/com/wechatprojectt/MainActivity.java
@@ -0,0 +1,13 @@
+package lxd.com.wechatprojectt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
